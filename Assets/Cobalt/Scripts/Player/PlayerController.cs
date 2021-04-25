9c5107301c393522cee4000169311506af3ccfea
tjm <tjm@nopath>
2021-04-25 11:51:13 +0100

    Player buoyancy.

--- Assets/Cobalt/Scripts/Player/PlayerController.cs
+++ Assets/Cobalt/Scripts/Player/PlayerController.cs
@@ -10,7 +10,13 @@ namespace Cobalt
 	{
 		public float MoveSpeed = 40.0f;
 		public float MoveSmoothTime = 0.5f;
+		public float MoveAccel = 80.0f;
 		public InputAction MoveAction;
+
+		public float NeutralDepth = 0.0f;
+		public float DepthWindowHalfSize = 40.0f;
+
+		public float VerticalDragFactor = 0.5f;
 
 		private void Start()
 		{
@@ -29,9 +35,20 @@ namespace Cobalt
 
 		private void FixedUpdate()
 		{
+			// Horizontally we just set a target velocity.
 			var targetVelocity = MoveDirection * MoveSpeed;
 			var velocity = m_rigidBody.velocity;
-			m_rigidBody.velocity = Vector2.SmoothDamp(m_rigidBody.velocity, targetVelocity, ref velocity, MoveSmoothTime);
+			var xVel = velocity.x;
+			velocity.x = Mathf.SmoothDamp(m_rigidBody.velocity.x, targetVelocity.x, ref xVel, MoveSmoothTime);
+			m_rigidBody.velocity = velocity;
+
+			// Vertically our movement is a combination of forces:
+			var depthWindowFactor = MoveAccel / DepthWindowHalfSize;
+			var springForceY = (NeutralDepth - transform.position.y) * depthWindowFactor;
+			var motiveForceY = MoveDirection.y * MoveAccel;
+			var dragForceY = -velocity.y * Mathf.Abs(velocity.y) * VerticalDragFactor;
+
+			m_rigidBody.AddForce(Vector2.up * (springForceY + motiveForceY + dragForceY));
 		}
 
 		private Vector2 MoveDirection => MoveAction.ReadValue<Vector2>();
